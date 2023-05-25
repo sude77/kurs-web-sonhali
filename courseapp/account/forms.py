e4595a98591f3916bcf7fbbeda58e2f09275d802
sude77 <firdevs06sude@gmail.com>
2023-05-25 21:36:10 +0300

    django kurs web sitesi

---
+++ courseapp/account/forms.py
@@ -0,0 +1,69 @@
+from typing import Any
+from django import forms
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.forms import AuthenticationForm,UserCreationForm,PasswordChangeForm
+from django.forms import widgets
+from django.contrib import messages
+from django.contrib.auth.models import User
+
+
+class LoginUserForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+       super().__init__(*args,**kwargs)
+       self.fields["username"].widget = widgets.TextInput(attrs={"class":"form-control"})
+       self.fields["password"].widget = widgets.PasswordInput(attrs={"class":"form-control"})
+
+
+    def clean_username(self):
+        username = self.cleaned_data.get("username")
+
+        if username == "admin":
+            messages.add_message(self.request,messages.SUCCESS, "hoş geldin admin")
+
+        return username  
+
+
+    # def confirm_login_allowed(self, user):
+    #     if user.username.startswith("s"):
+    #         raise forms.ValidationError("bu kullanıcı adı ile login olamazsınız.")
+        
+
+
+class NewUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ("username","email","first_name","last_name",)
+
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args,**kwargs)
+        self.fields["password1"].widget = widgets.PasswordInput(attrs={"class":"form-control"})
+        self.fields["password2"].widget = widgets.PasswordInput(attrs={"class":"form-control"})
+        self.fields["username"].widget = widgets.TextInput(attrs={"class":"form-control"})
+        self.fields["email"].widget = widgets.EmailInput(attrs={"class":"form-control"})
+        self.fields["first_name"].widget = widgets.EmailInput(attrs={"class":"form-control"})
+        self.fields["last_name"].widget = widgets.EmailInput(attrs={"class":"form-control"})
+        self.fields["email"].required = True
+
+    
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+
+        if User.objects.filter(email = email).exists():
+            self.add_error("email","email daha önce kullanılmış")
+
+        return email
+    
+
+
+class UserPasswordChangeForm(PasswordChangeForm):
+   
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args,**kwargs)
+        self.fields["new_password1"].widget = widgets.PasswordInput(attrs={"class":"form-control"})
+        self.fields["new_password2"].widget = widgets.PasswordInput(attrs={"class":"form-control"})
+        self.fields["old_password"].widget = widgets.PasswordInput(attrs={"class":"form-control"})
+      
+   
+
---
+++ courseapp/courses/forms.py
@@ -0,0 +1,71 @@
+from django import forms
+
+from courses.models import Course
+
+# class CourseCreateForm(forms.Form):
+#     title = forms.CharField(
+#         label="kurs başlığı",
+#         required=True,
+#         error_messages={
+#             "required":"kurs başlığı girmelisiniz."},
+#         widget=forms.TextInput(attrs={"class":"form-control"}))
+    
+
+#     description = forms.CharField(widget=forms.Textarea(attrs={"class":"form-control"}))
+#     imageUrl = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control"}))
+#     slug = forms.SlugField(widget=forms.TextInput(attrs={"class":"form-control"}))
+
+class CourseCreateForm(forms.ModelForm):
+    class Meta:
+        model = Course
+        fields = ('title','description','image','slug')
+        labels = {
+            'title':"kurs başlığı",
+            'description': 'açıklama'
+        }
+        widgets = {
+            "title":forms.TextInput(attrs={"class":"form-control"}),
+            "description":forms.Textarea(attrs={"class":"form-control"}), 
+            "slug":forms.TextInput(attrs={"class":"form-control"}),
+            
+        }
+        error_messages = {
+            "title":{
+                "required":"kurs başlığı girmelisiniz.",
+                "max_length":"maksimum 50 karakter girmelisiniz."
+            },
+            "description":{
+                "required":"kurs açıklaması gereklidir."
+            }
+        }
+
+
+
+class CourseEditForm(forms.ModelForm):
+    class Meta:
+        model = Course
+        fields = ('title','description','image','slug','categories','isActive')
+        labels = {
+            'title':"kurs başlığı",
+            'description': 'açıklama'
+        }
+        widgets = {
+            "title":forms.TextInput(attrs={"class":"form-control"}),
+            "description":forms.Textarea(attrs={"class":"form-control"}),
+            "slug":forms.TextInput(attrs={"class":"form-control"}),
+            "categories": forms.SelectMultiple(attrs={"class":"form-control"})
+        }
+        error_messages = {
+            "title":{
+                "required":"kurs başlığı girmelisiniz.",
+                "max_length":"maksimum 50 karakter girmelisiniz."
+            },
+            "description":{
+                "required":"kurs açıklaması gereklidir."
+            }
+        }
+
+
+
+class UploadForm(forms.Form):
+    image = forms.ImageField()
